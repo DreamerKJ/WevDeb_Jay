59bac6c0c052626a0505a5105afa03496c4b20ed
DreamerKJ <meadowdreamer1@gmail.com>
2025-04-09 18:38:58 -0400

    first draft(just the list)

---
+++ Week11/HW/Tue/script.js
@@ -0,0 +1,4 @@
+const listitem = document.getElementById("listitem");
+const groceries = ["Bell . Peppers\n", "Ground . Beef\n", "Salt\n", "Sausages\n", "Sliced Cheese\n", "Noodles\n"]
+
+listitem.textContent = groceries
