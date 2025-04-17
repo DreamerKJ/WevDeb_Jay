54b9369fc8cf3fbad1d0a0e78d4a7afe4120accc
DreamerKJ <meadowdreamer1@gmail.com>
2025-04-17 09:49:50 -0400

    heh

---
+++ Week12/Class/Thu/library.js
@@ -0,0 +1,19 @@
+let curWeek; // Constantly updating to current week.
+let events; // Array of events
+
+function highlightDates() {
+    
+} // Sets the calendar to show which days of week has events.
+
+function nextWeek() {
+    currWeek++;
+}
+function prevWeek() {
+    currWeek--;
+} // Updates curWeek
+
+function showEventPage() {
+    
+} // Updates page to show specific event 
+
+highlightDates();
